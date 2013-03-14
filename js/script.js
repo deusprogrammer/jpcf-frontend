cf177dc80a8ddd921442b44070a81314a26f2c99
Michael Main <mike.main@rackspace.com>
2013-03-14 17:10:31 -0500

    Added new features to front page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var jpcfUrl = "http://jpcf-church.no-ip.org:8080";
+var testUrl = "http://localhost:8080";
+var blogUser = "deusprogrammer";
 var events = new Array();
+var blogs  = new Array();
 var eventIndex = 0;
 var currentPage;
 var lastActive;
@@ -29,6 +32,9 @@ function loadPage(page) {
          $("table.podcast-table td.podcast-name").css("width", "200px");
          $("table.podcast-table td.podcast-description").css("width", "400px");
          $("table.podcast-table th").css("color", "white").css("font-size: 25px;");
+      } else if (page == "main") {
+         writeEvents();
+         writeBlogs();
       }
    });
    
@@ -63,7 +69,25 @@ function loadSlideShowImages() {
    jQuery.ajaxSetup({async:true});
 }
 
-function loadBannerEvents() {
+function loadLatestBlogs(user, limit) {
+   jQuery.ajaxSetup({async:false});
+   jQuery.get(
+      testUrl + "/Clogger/js/" + user + "/summary",
+      {limit: limit},
+      function(data) {
+         console.log("Call successful!");
+         for (var i in data) {
+            console.log("BLOG: " + data[i].title + ": " + data[i].content + "(" + data[i].dateCreated + ")");
+            var blog = {title: data[i].title, content: data[i].content, dateCreated: data[i].dateCreated, link: data[i].link};
+            blogs.push(blog);
+         }
+      },
+      "json"
+   );
+   jQuery.ajaxSetup({async:true});
+}
+
+function loadEvents() {
    jQuery.ajaxSetup({async:false});
    jQuery.get(
       jpcfUrl + "/jpcf/JS/listEvents.json",
@@ -97,6 +121,36 @@ function loadPodcasts() {
    jQuery.ajaxSetup({async:true});
 }
 
+function writeEvents() {
+    console.log("IN WRITE EVENTS()");
+    
+    if (events.length == 0) {
+        $('div#upcoming-events').html("None");        
+    } else {
+        $('div#upcoming-events').html("<ul>");
+        for (var i in events) {
+            console.log("WRITING EVENT: " + events[i].name);
+            $('div#upcoming-events').append("<li><b>" + events[i].startDate + "</b>- " + events[i].name + "</li>");
+        }
+        $('div#upcoming-events').append("</ul>");
+    }
+}
+
+function writeBlogs() {
+    console.log("IN WRITE BLOGS()");
+    if (blogs.length == 0) {
+        $('div#latest-blogs').html("None");       
+    } else {
+        $('div#latest-blogs').html("<ul>");
+        for (var i in blogs) {
+            console.log("WRITING BLOG: " + blogs[i].title);
+            $('div#latest-blogs').append("<li><a class='blog-link' href='" + blogs[i].link + "'><b>" + blogs[i].title + "</b></a> (" + blogs[i].dateCreated + ")</li>");
+        }
+        $('div#latest-blogs').append("</ul>");
+        $('div#latest-blogs').append("<a class='blog-link' href='" + testUrl + "/Clogger/user/" + blogUser + "/blogs'>See All Blog Posts</a>");
+    }
+}
+
 $(function(){
    console.log("Starting!");
    if ($.cookie("currentPage")) {
@@ -108,8 +162,13 @@ $(function(){
    }
    
    loadSlideShowImages();
-   loadBannerEvents();
+   loadEvents();
+   loadLatestBlogs(blogUser, 3);
    loadPage(currentPage);
+   
+   if (currentPage === "main") {
+
+   }
 
    if (events.length != 0) {
       setBannerEvent(events[eventIndex]);
@@ -146,7 +205,6 @@ $(function(){
 
 	var slider=$('.slider');
 	slider.tms({
-			 //nameMask:'images/image*.jpg,1-21',
 			 playBu:'.play',
 			 slideShow:20000,
 			 duration:1000,
